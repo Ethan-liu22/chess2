68f51992b82deec4a6455fc0ff26badf8654956b
Ethan-liu22 <80666291+Ethan-liu22@users.noreply.github.com>
2021-03-22 21:06:05 -0700

    Update App.js

--- App.js
+++ App.js
@@ -12,7 +12,21 @@ import Board from './Board'
 function App() {
   return (
     <Fragment>
-      <Board knightPosition={[7, 0]} bishopPosition={[4, 2]} rookposition={[3, 6]} queenposition={[1, 7]} kingposition={[0, 5]} pawnposition={[6, 4]}/>
+      <Board knightPosition={[1, 0]} bishopPosition={[2, 7]} rookPosition={[0, 7]} queenPosition={[3, 7]} kingPosition={[4, 7]} pawnPosition={[0, 6]}/>
+
+      <table>
+        <thead>
+          <tr>
+            <th colspan="2">The table header</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+          </tr>
+        </tbody>
+      </table>
       <div className="whitewin">
         White wins
       </div>
@@ -29,3 +43,6 @@ function App() {
   );
 }
 export default App;
+
+
+
